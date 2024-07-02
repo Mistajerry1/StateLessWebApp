03415137ef1fd45e96bfa209a7f5a2f979892e5c
Mistajerry1 <168672771+Mistajerry1@users.noreply.github.com>
2024-07-02 15:19:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.web_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" 
 }
 
 resource "aws_lb" "web_alb" {
